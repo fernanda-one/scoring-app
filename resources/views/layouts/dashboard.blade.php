b7b16ddbb47af22d308739622fcf1783d658e482
anangsyah <anang.haqim@ist.id>
2023-10-15 17:48:26 +0700

    fix: bug

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -58,7 +58,7 @@
                             </li>
                         </ul>
                     </div>
-                    <div class="fixed left-0 bottom-0">
+                    {{-- <div class="fixed left-0 bottom-0">
                         <ul class="w-64 font-medium border-t border-gray-200 dark:border-gray-700">
                             <li>
                                 <a href="/logout" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
@@ -69,7 +69,7 @@
                                 </a>
                             </li>
                         </ul>
-                    </div>
+                    </div> --}}
                 </div>
             </div>
         </div>
@@ -118,6 +118,18 @@
                         </defs>
                     </svg>
                     <span class="flex-1 ml-3 whitespace-nowrap">History</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="absolute bottom-0">
+        <ul class="w-64 font-medium border-t border-gray-200 dark:border-gray-700">
+            <li>
+                <a href="/logout" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"/>
+                    </svg>
+                    <span class="flex-1 ml-3 whitespace-nowrap">Logout</span>
                 </a>
             </li>
         </ul>
