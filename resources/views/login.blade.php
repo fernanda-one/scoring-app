ee85f21c52dd7c3e87972149043f06291f651363
fernanda-one <fernandaiqshal111@gmail.com>
2024-06-14 14:11:25 +0700

    fix: styling code

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,20 +3,25 @@
 @section('container')
     <section class="bg-gray-50 dark:bg-gray-900">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
-            <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700 drop-shadow-lg">
+            <div
+                class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700 drop-shadow-lg">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                     <div class="flex justify-center">
                         <a href="#" class="flex items-center my-4 text-2xl font-semibold text-gray-900 dark:text-white">
-                            <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo">
+                            <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+                                alt="logo">
                             Pencak Guru
                         </a>
                     </div>
                     <form class="space-y-4 md:space-y-6" action="/login" method="post">
                         @csrf
-                        @if(session('error'))
-                            <div class="flex items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
-                                <svg class="flex-shrink-0 inline w-4 h-4 mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                        @if (session('error'))
+                            <div class="flex items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
+                                role="alert">
+                                <svg class="flex-shrink-0 inline w-4 h-4 mr-3" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path
+                                        d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
                                 </svg>
                                 <span class="sr-only">Info</span>
                                 <div>
@@ -26,25 +31,34 @@
                         @endif
 
                         <div>
-                            <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama Pengguna</label>
-                            <input type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                            <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama
+                                Pengguna</label>
+                            <input type="text" name="username" id="username"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                required="">
                         </div>
                         <div>
-                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kata Sandi</label>
-                            <input type="password" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kata
+                                Sandi</label>
+                            <input type="password" name="password" id="password"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                required="">
                         </div>
                         <div class="flex items-center justify-between">
                             <div class="flex items-start">
                                 <div class="flex items-center h-5">
-                                    <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800">
+                                    <input id="remember" aria-describedby="remember" type="checkbox"
+                                        class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800">
                                 </div>
                                 <div class="ml-3 text-sm">
                                     <label for="remember" class="text-gray-500 dark:text-gray-300">Ingat saya</label>
                                 </div>
                             </div>
-                            <a href="#" class="text-sm font-medium text-blueDefault hover:underline">Lupa Password?</a>
+                            <a href="#" class="text-sm font-medium text-blueDefault hover:underline">Lupa
+                                Password?</a>
                         </div>
-                        <button type="submit" class="w-full text-white bg-blueDefault hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Masuk</button>
+                        <button type="submit"
+                            class="w-full text-white bg-blueDefault hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Masuk</button>
                     </form>
                 </div>
             </div>
--- resources/views/scoring.blade.php
+++ resources/views/scoring.blade.php
@@ -1,30 +1,31 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Scoring</title>
 </head>
+
 <body>
 
-<span id="redScore">0</span>
-<span id="blueScore">0</span>
-<input id="user" type="text" hidden="hidden" value="{{auth()->user()}}">
+    <span id="redScore">0</span>
+    <span id="blueScore">0</span>
+    <input id="user" type="text" hidden="hidden" value="{{ auth()->user() }}">
 
-<form action="" id="pukul">
-    <input id="gelanggang_id" type="number" hidden="" value="{{auth()->user()->gelanggang_id??1}}">
-    <button type="submit">Pukulan</button>
-</form>
-<form action="" id="tendang">
-    <input id="gelanggang_id" type="number" hidden="" value="{{auth()->user()->gelanggang_id??1}}">
-    <button type="submit">Tendangan</button>
-</form>
+    <form action="" id="pukul">
+        <input id="gelanggang_id" type="number" hidden="" value="{{ auth()->user()->gelanggang_id ?? 1 }}">
+        <button type="submit">Pukulan</button>
+    </form>
+    <form action="" id="tendang">
+        <input id="gelanggang_id" type="number" hidden="" value="{{ auth()->user()->gelanggang_id ?? 1 }}">
+        <button type="submit">Tendangan</button>
+    </form>
 
-<script src="{{ mix('js/scoringJuri.js') }}">
-</script>
-<script src="{{ mix('js/scoreUpdate.js') }}">
-</script>
+    <script src="{{ mix('js/scoringJuri.js') }}"></script>
+    <script src="{{ mix('js/scoreUpdate.js') }}"></script>
 </body>
+
 </html>
